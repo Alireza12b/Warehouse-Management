d70a85f2ca5751c69de7b6275144270ef747ac4b
Alireza Bagheri <alirezabagheri12b@gmail.com>
2023-06-03 23:38:39 +0330

    Product entity added

---
+++ WarehouseOperations/Domain/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseOperations.Domain
+{
+    public class Product
+    {
+        int ProductId { get; set; }
+        public string Name { get; set; }
+        public Guid Barcode { get; set; }
+    }
+}
