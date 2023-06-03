d96c77c6912ff94407940991fe5a2e6e6824a07f
Alireza Bagheri <alirezabagheri12b@gmail.com>
2023-06-03 23:42:28 +0330

    Stock entity added

---
+++ WarehouseOperations/Domain/Stock.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseOperations.Domain
+{
+    public class Stock
+    {
+        Guid StockId { get; set; }
+        string Name { get; set; }
+        int ProductId { get; set; }
+        int ProductQuantity { get; set; }
+        int ProductPrice { get; set; }
+    }
+}
