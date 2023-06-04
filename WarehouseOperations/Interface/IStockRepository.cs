cba9d0d387dea718660f798bb4ca731b14bc363f
Alireza Bagheri <alirezabagheri12b@gmail.com>
2023-06-04 20:39:56 +0330

    StockRepository service and class created

---
+++ WarehouseOperations/Interface/IStockRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WarehouseOperations.Domain;
+
+namespace WarehouseOperations.Interface
+{
+    public interface IStockRepository
+    {
+        string SaleProduct(int productId, int cnt);
+        string BuyProduct(Stock productInStock);
+        List<Stock> GetSalesProductList();
+    }
+}
---
+++ WarehouseOperations/Services/StockRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WarehouseOperations.Domain;
+using WarehouseOperations.Interface;
+
+namespace WarehouseOperations.Services
+{
+    public class StockRepository : IStockRepository
+    {
+        public string BuyProduct(Stock productInStock)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Stock> GetSalesProductList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string SaleProduct(int productId, int cnt)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
